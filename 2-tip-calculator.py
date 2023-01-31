38354a0e648fe0fff49786bb530c655756a3f3e3
Abazhiz <abdulazizrealme8@gmail.com>
2023-01-31 11:28:13 +0700

    improve existing files

--- 2-tip-calculator.py
+++ 2-tip-calculator.py
@@ -86,17 +86,21 @@ Day 2 Project: Tip Calculator
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill?"))
+people = int(input("How many people to split the bill? "))
 
 tip_as_percent = tip / 100
-print(f'tip_as_percent = tip / 100 = {tip_as_percent}')
+print(f'\ntip_as_percent = tip / 100 = {tip_as_percent}')
 total_tip_amount = bill * tip_as_percent
 print(f'total_tip_amount = bill * tip_as_percent = {total_tip_amount}')
 total_bill = bill + total_tip_amount
 print(f'total_bill = bill + total_tip_amount = {total_bill}')
+
 bill_per_person = total_bill / people
-print(f'bill_per_person = total_bill / people = {bill_per_person}')
-final_amount = round(bill_per_person, 2)       # Change value round to 2 decimal places
+print(f'\nbill_per_person = total_bill / people = {bill_per_person}')
+final_amount = round(bill_per_person, 2)
+print(f'final_amount = round(bill_per_person, 2) = {final_amount}')
+final_amount = '{:.2f}'.format(bill_per_person)
+# print(f'final_amount = \'\{:.2f\}\'.format(bill_per_person)')
 print(f'final_amount = round(bill_per_person, 2) = {final_amount}')
 
 
